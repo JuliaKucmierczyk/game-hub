f3e214786fb3fc58b062c9727cfb5463f84c83b8
Julia <july.kucmierczyk@gmail.com>
2023-12-16 14:23:32 +0100

    Refactor code and update dependencies

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,7 +13,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>error</Text>;
 
   return (
     <>
@@ -28,7 +28,7 @@ const GameGrid = ({ gameQuery }: Props) => {
               <GameCardSkieleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
+        {data?.results.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import apiClient from '../services/api-client'
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from './useData';
+import { FetchResponse } from '../services/api-client';
 import genres from '../data/genres';
 
 export interface Genre {
