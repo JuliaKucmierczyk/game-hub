d024f1060ad277e8dce0e84dfe25f1d0366313ec
Julia <july.kucmierczyk@gmail.com>
2024-01-06 18:29:32 +0100

    Add React Router DOM dependency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,22 @@
-import { Flex, Grid, GridItem, Box, Show } from "@chakra-ui/react";
-import NavBar from "./components/NavBar";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
-import GameHeading from "./components/GameHeading";
+import { Grid, Show, GridItem, Flex, Box } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`, // 1024px
+        base: ` "main"`,
+        lg: ` "aside main"`, // 1024px
       }}
       templateColumns={{
         base: "1fr",
         lg: "250px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -41,6 +37,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
